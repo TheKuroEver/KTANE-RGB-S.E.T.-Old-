00ab6eb5f6b1f178de88c92a1e547301092e5934
TheKuroEver <mariusztang@outlook.com>
2023-01-25 11:04:04 +0000

    Added smooth transitions for cubes

--- Assets/RGBSet.cs
+++ Assets/RGBSet.cs
@@ -13,13 +13,15 @@ public class RGBSet : MonoBehaviour
     public KMBombInfo Bomb;
     public KMAudio Audio;
 
-    static int ModuleIdCounter = 1;
-    int ModuleId;
+    private static int ModuleIdCounter = 1;
+    private int ModuleId;
     private bool ModuleSolved;
 
     public KMSelectable[] ColouredButtons;
     public KMSelectable[] StageLights;
     public TextMesh ScreenText;
+
+    private const float bigCubeSize = 0.03173903f;
 
     void Awake() {
         ModuleId = ModuleIdCounter++;
@@ -37,10 +39,46 @@ public class RGBSet : MonoBehaviour
 
     void ButtonPress(KMSelectable button)
     {
-        float scale = Rnd.Range(0, 3)*0.25f + 0.5f;
+        float newScale = Rnd.Range(0, 3)*0.25f + 0.5f;
         ScreenText.text = button.name;
-        button.GetComponent<MeshRenderer>().material.color = GetRandomTernaryColour();
-        button.GetComponent<Transform>().localScale = new Vector3(0.03173903f * scale, 0.03173903f * scale, 0.03173903f * scale);
+        StartCoroutine(SmoothCubeScale(button, newScale));
+        StartCoroutine(SmoothCubeColourTransition(button, GetRandomTernaryColour()));
+    }
+
+    IEnumerator SmoothCubeScale(KMSelectable cube, float newScale)
+    {
+        float delta = 0;
+        float scaleSeconds = 1;
+        float scaleDifference = bigCubeSize * newScale - cube.transform.localScale.x;
+        float sizeDelta;
+
+        while (delta < scaleSeconds)
+        {
+            delta += Time.deltaTime;
+            sizeDelta = (Time.deltaTime / scaleSeconds) * scaleDifference;
+            cube.transform.localScale += new Vector3(sizeDelta, sizeDelta, sizeDelta);
+            yield return null;
+        }
+    }
+
+    IEnumerator SmoothCubeColourTransition(KMSelectable cube, Color newColour)
+    {
+        Color currentColor = cube.GetComponent<MeshRenderer>().material.color;
+        float delta = 0;
+        float transitionSeconds = 1;
+        float redDifference = newColour.r - currentColor.r;
+        float greenDifference = newColour.g - currentColor.g;
+        float blueDifference = newColour.b - currentColor.b;
+        float colourDeltaMultiplier;
+
+        while (delta < transitionSeconds)
+        {
+            delta += Time.deltaTime;
+            colourDeltaMultiplier = (Time.deltaTime / transitionSeconds);
+            cube.GetComponent<MeshRenderer>().material.color = new Color(currentColor.r + colourDeltaMultiplier * redDifference, currentColor.g + colourDeltaMultiplier * greenDifference, currentColor.b + colourDeltaMultiplier * blueDifference);
+            currentColor = cube.GetComponent<MeshRenderer>().material.color;
+            yield return null;
+        }
     }
 
     void StageLightPress(KMSelectable light)
