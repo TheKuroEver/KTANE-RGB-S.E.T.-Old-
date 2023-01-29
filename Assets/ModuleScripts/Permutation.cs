5cb88d123cd2d4697b9c3bca649e9733525880b1
TheKuroEver <mariusztang@outlook.com>
2023-01-29 14:53:14 +0000

    Started work on Permutation file

--- Assets/ModuleScripts/Permutation.cs
+++ Assets/ModuleScripts/Permutation.cs
@@ -2,10 +2,78 @@
 using System.Collections.Generic;
 using rnd = UnityEngine.Random;
 
-public static class Permutation
+public static class PermsManager
 {
+    private static int[] permutation;
+    private static List<Cycle> cycles;
+
     public static void GenerateRandomPermutationSequence()
     {
+        cycles = new List<Cycle>();
+        permutation = GetRandomPermutation();
+        
+        SeperateIntoDisjointCycles();
+    }
 
+    private static void SeperateIntoDisjointCycles()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            if (permutation[i] != -1 && permutation[i] != i) cycles.Add(FindCycle(i));
+        }
+    }
+
+    private static int[] GetRandomPermutation()
+    {
+        var startList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+        var endList = new List<int>();
+        int position;
+
+        for (int i = 0; i < 9; i++)
+        {
+            position = rnd.Range(0, startList.Count);
+            endList.Add(startList[position]);
+            startList.RemoveAt(position);
+        }
+
+        return endList.ToArray();
+    }
+
+    private static Cycle FindCycle(int startPosition)
+    {
+        var tempCycles = new List<int>() { permutation[startPosition] };
+        permutation[startPosition] = -1;
+
+        while (permutation[tempCycles[tempCycles.Count - 1]] != -1)
+        {
+            tempCycles.Add(permutation[tempCycles[tempCycles.Count - 1]]);
+            permutation[tempCycles[tempCycles.Count - 1]] = -1;
+        }
+
+        return new Cycle(tempCycles.ToArray());
+    }
+
+    private class Cycle
+    {
+        private int[] _elements;
+
+        public Cycle(int[] elements)
+        {
+            _elements = elements;
+        }
+
+        public int Permute(int number)
+        {
+            int counter = 0;
+            foreach (int element in _elements)
+            {
+                if (element == number)
+                {
+                    return _elements[(counter + 1) % _elements.Length];
+                }
+            }
+
+            throw new System.InvalidOperationException("Element not in cycle.");
+        }
     }
 }
