3ef66ff2dd81351c8b1626ed37ea1c0d37bbb512
TheKuroEver <mariusztang@outlook.com>
2023-01-30 19:24:18 +0000

    what 2

---
+++ Assets/ClassesScript.cs
@@ -0,0 +1,29 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class ColouredCube
+//{
+//    private const float _positionOffset = 0.04f;
+
+//    private GameObject _cube;
+//    private Vector3 _position;
+
+//    public Vector3 Position { get { return _position; } }
+
+//    public KMSelectable Button { get { return _cube.GetComponent<KMSelectable>(); } }
+
+//    public ColouredCube(GameObject cube, Vector3 position, Color colour)
+//    {
+//        _cube = cube;
+//        _position = position;
+
+//        _cube.transform.position = _positionOffset * _position;
+//        _cube.GetComponent<MeshRenderer>().material.color = colour;
+//    }
+
+//    private void CubePress()
+//    {
+//        return;
+//    }
+//}
